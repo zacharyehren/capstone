5cd86196349e449f497c29a7808f6433036192f2
Zachary Ehren <zachehren@gmail.com>
2017-09-16 15:25:17 -0700

    begin list tickets

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -2,6 +2,9 @@
     function HomeCtrl(GoogleOauth, ZenFactory) {
         ZenFactory.zendeskTickets();
 
+        // this.listSubjects = ZenFactory.subjects;
+        //
+        // console.log(this.listSubjects);
 
         this.signOut = function() {
           GoogleOauth.signOut();
